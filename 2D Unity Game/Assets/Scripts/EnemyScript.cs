948909f1c00a40cae513b145f28365b266167530
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-09-26 15:57:37 +0200

    Added movement

---
+++ 2D Unity Game/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    private Vector3 startposition = new Vector3(1, -0.3f, 0);
+    private Vector3 endposition = new Vector3(-1, -0.3f, 0);
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        //rb.AddForce(new Vector2(0, Mathf.Sin(speed * Time.deltaTime * 1) - 10f / 1f));
+        transform.position = Vector3.Lerp(startposition, endposition, (Mathf.Sin(speed * Time.time) + 0.1f) / 0.1f);
+        //rb.AddTorque(0.00002f, ForceMode2D.Force);
+        //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, (Mathf.Cos(2f * Time.deltaTime) + 0.1f), transform.rotation.w);
+    }
+}
---
+++ 2D Unity Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //[SerializeField] private Scrollbar staminaBar;
+    //[SerializeField] private float stamina = 15f;
+    [SerializeField] private float runSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    bool enemyHit = false;
+    Vector2 playerPosition;
+
+    // Jump
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private float jumpingPower = 12f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        playerPosition = rb.position;
+    }
+
+    void Update()
+    {
+        Move();
+        FlipSprite();
+
+        if (enemyHit == true)
+        {
+            rb.position = playerPosition;
+        }
+    }
+
+    public bool IsGrounded() 
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+
+    void Move()
+    {
+
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
+
+        if (moveInput.y > 0 && IsGrounded())
+        {
+            playerVelocity = new Vector2(moveInput.x, jumpingPower);
+        }
+
+        rb.velocity = playerVelocity;
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), transform.localScale.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemyHit = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemyHit = false;
+        }
+    }
+}
